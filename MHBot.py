3019cfe32c63599aee9f96120ff52c0a7f8dbe2d
alochimpasplum <lagoscarrera@gmail.com>
2020-09-12 13:36:59 +0200

    Primeros settings del proyecto

--- MHBot.py
+++ MHBot.py
@@ -4,14 +4,14 @@ from config.auth import token
 import logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger('AchicaynaBot')
+logger = logging.getLogger('MH_Bot')
 
 
 def start(bot, update):
     logger.info('He recibido un comando start')
     bot.send_message(
         chat_id=update.message.chat_id,
-        text="Soy un Achicayna, que entre los primeros pobladores de Canarias era el equivalente a un plebeyo."
+        text="¡Miáumonos de caza!"
     )
 
 
